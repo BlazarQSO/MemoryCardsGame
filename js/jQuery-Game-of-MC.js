5dd6dfbe7dfc5fb96e2a744d31626876c46f305e
BlazarQSO <BlazarQSO@gmail.com>
2019-06-28 05:46:45 +0300

    Create a marking of page and styles for it

---
+++ js/jQuery-Game-of-MC.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+    $("button")
+        .button()
+        .click(function (e) {
+            
+        });
+
+    $("select")
+        .selectmenu()
+        .click(function (e) {
+
+        });
+    
+    //$('section').css({ 'background-image': 'url(ui-bg_3D-boxes_100_ae9029_12x10.png)' });
+
+//    $("section").css("background-image", "url(images/ui-bg_3D-boxes_100_ae9029_12x10.png");
+    //.tabs()
+    //.progressbar({ value: 0 })
+        
+    
+    
+});
