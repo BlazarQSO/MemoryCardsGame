158b7390d499294132f7508181b50d543b0abe91
BlazarQSO <BlazarQSO@gmail.com>
2019-06-29 21:53:20 +0300

    Create startGame function

--- js/jQuery-Game-of-MC.js
+++ js/jQuery-Game-of-MC.js
@@ -1,5 +1,6 @@
 ﻿$(function () {
-    $("button")
+    // Initialization of jQuery-ui library controls.
+    $("button, a")
         .button()
         .click(function (e) {
             
@@ -11,12 +12,76 @@
 
         });
     
-    //$('section').css({ 'background-image': 'url(ui-bg_3D-boxes_100_ae9029_12x10.png)' });
-
-//    $("section").css("background-image", "url(images/ui-bg_3D-boxes_100_ae9029_12x10.png");
-    //.tabs()
-    //.progressbar({ value: 0 })
+    // Objects of controls
+    var actions = {        
+        "start": {
+            title: "Start",
+            onClickHandler: function (e) {
+                return function () {
+                    startGame();
+                }
+            }
+        },
+        "opt": {
+            title: "Options",
+            onClickHandler: function (e) {
+                return function () {
+                    
+                }
+            }
+        },        
+    }
+    
+    for (var key in actions) {
+        document.getElementById(key).onclick = actions[key].onClickHandler();
+    }
         
-    
-    
+    function startGame() {
+        var name = $("#selName").val();
+        var lvl = $("#selLvl").val();
+
+        $("#lg").text(name);
+        $("#ll").text(": Level №" + lvl[0]);
+
+        var input = $("#game");
+        
+        var horiz = Math.floor((100 - lvl[5] - 1) / lvl[5]);
+        var vert = Math.floor((100 - lvl[3] - 3)/ lvl[3]);        
+
+        for (var j = 1; j <= lvl[3]; j++) {
+            for (var i = 1; i <= lvl[5]; i++) {
+                
+                $("<div/>", {
+                    text: "face",
+                    id: "c" + j + i,
+                    position: 1,
+                    "class": "card",
+                    click: function () {      
+                        if ($(this).text() == "face") {
+                            $(this).css("background-image", "url(/css/images/shirt-2-min.png)");                            
+                            $(this).text("back");
+                        }
+                        else {
+                            $(this).css("background-image", "url(/css/images/Kc-1.png)");                            
+                            $(this).text("face");
+                        }
+                    }
+                }).appendTo("#game");
+            }
+            input.append("<br />");
+        }
+
+        for (var j = 1; j <= lvl[3]; j++) {
+            for (var i = 1; i <= lvl[5]; i++) {
+                var temp = $("#c" + j + i);
+                temp.css("margin-left", "1%");
+                temp.css("margin-bottom", "0.5%");
+                temp.css("width", horiz + "%");
+                temp.css("height", vert + "%");
+                temp.css("border", "2px solid black");
+                temp.css("background-image", "url(/css/images/Kc-1.png)");
+            }
+        }        
+    }
+
 });
